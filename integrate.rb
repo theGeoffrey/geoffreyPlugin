04045a8c0b174e4108d8489afc364085f69ebd90
anoukruhaak <anoukruhaak@gmail.com>
2014-11-27 11:17:55 -0600

    creates working plugin for sending data to Geoffrey

--- integrate.rb
+++ integrate.rb
@@ -1,11 +1,21 @@
-module Jobs
-	class SendUserGeof < Jobs::Base
+require 'net/http'
 
-		def execute
-			uri = URI.parse("http://geoffrey.co/users/add")
+module Jobs
+	class SendToGeof < Jobs::Base
+
+		def initialize
+		end
+
+		def execute(args)
+			api_key = 'yay'
+			address = '192.168.1.126'
+
+			method = args['method']
+
+			uri = URI.parse("http://#{address}:8080/api#{method}?key=#{api_key}")
+			http = Net::HTTP.new(uri.host, uri.port)
 			request = Net::HTTP::Post.new(uri.request_uri)
-			request.set_form_data("key" => "yay", "email" => email)
-			response = http.request(request)
+			response = http.request(request, JSON.dump(args))
 			json = JSON.parse(response.body)
 
 			if json["succeed"] != true
@@ -15,16 +25,33 @@ module Jobs
 	end
 end
 
-module GeoffreyPlugin
+
+
+module GeoffreyPluginUser
 
 	def self.included(klass)
 		klass.after_create :add_user_geof
 	end
 
 	def add_user_geof
-		Jobs.enqueue(:send_user_geof)
+		method = '/users/new'
+		Jobs.enqueue(:send_to_geof, email: email, user: id, method: method)
 		
 	end
 end
 
-User.send :include, GeoffreyPlugin
+module GeoffreyPluginPost
+
+	def self.included(klass)
+		klass.after_create :add_post_geof
+	end
+
+	def add_post_geof
+		method = '/posts/new'
+		Jobs.enqueue(:send_to_geof, post: id, method: method)
+		
+	end
+end
+
+User.send :include, GeoffreyPluginUser
+Post.send :include, GeoffreyPluginPost
